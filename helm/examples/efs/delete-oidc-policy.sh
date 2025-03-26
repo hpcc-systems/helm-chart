268305eafbac1a36a5144e3310ad68d74e1d38f7
Gordon Smith <GordonJSmith@gmail.com>
2025-03-26 20:17:58 +0000

    HPCC Helm Charts 9.10.16-1
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- helm/examples/efs/delete-oidc-policy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-aws iam list-open-id-connect-providers
-# aws iam delete-open-id-connect-provider --open-id-connect-provider-arn <open-id-connect-provider>
--- helm/examples/efs/delete-role.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WORK_DIR=$(dirname $0)
-
-source ${WORK_DIR}/efs-env
-
-aws iam detach-role-policy --role-name ${EKS_NAME}_EFS_CSI_Role \
-  --policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AmazonEKS_EFS_CSI_Driver_Policy
-aws iam delete-role --role-name ${EKS_NAME}_EFS_CSI_Role
